3cb420e9ca4af22d4f6ed0eb46a71cf310fb9451
MyBus Build <autobuild@mybus-lite.com>
2026-06-29 16:23:26 +0800

    fix: 修复 LineDetailActivity 缺少 View import

--- app/src/main/java/com/mygolbs/mybus/LineDetailActivity.kt
+++ app/src/main/java/com/mygolbs/mybus/LineDetailActivity.kt
@@ -3,6 +3,7 @@ package com.mygolbs.mybus
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
